89d966c2cec05342cde4175108e0be605482c396
Andreas Schaefer <AndiSHFR@users.noreply.github.com>
2019-04-23 11:08:22 +0200

    Create set-proxy.sh

---
+++ scripts/set-proxy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+PROXY=http://proxy.corporate.lan:8080
+proxy=http://proxy.corporate.lan:8080
+export PROXY
+export proxy
+#
+HTTPS_PROXY=http://proxy.corporate.lan:8080
+https_proxy=http://proxy.corporate.lan:8080
+export HTTPS_PROXY
+export https_proxy
+#
